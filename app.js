50f1e677ea1913386ee1fce0a65e4db85d7b8016
feb2k <febriadipratama@gmail.com>
2017-05-31 21:19:26 +0800

    added RESTful routes

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express     = require("express"),
     bodyParser  = require("body-parser"),
-    mongoose    = require('mongoose'),
+    mongoose    = require("mongoose"),
+    method      = require("method-override"),
     app         = express();
 
-    mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds157971.mlab.com:57971/node_exercise');
+    mongoose.connect('mongodb://node_exercise:12345678!!!@ds157971.mlab.com:57971/node_exercise');
 
 // middleware
+    app.use(method("_method"));
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(express.static("public"));
     app.set("view engine", "ejs");
@@ -19,9 +21,9 @@ var express     = require("express"),
     var Campground = mongoose.model("Campground", campSchema);
 
     // Campground.create({
-    //   title: "Bukit beranjing",
+    //   title: "Bukit berkucing",
     //   image: "http://www.photosforclass.com/download/2602356334",
-    //   description: "kucing lemah minggir!! "
+    //   description: "anjing lemah minggir!! "
     // },
     //  function(err, camp){
     //   if(err){
@@ -59,9 +61,69 @@ app.get("/campgrounds/new", function(req, res){
 
 
 // CREATE ROUTE
-// app.post("/campgrounds", function(req, res){
-//
-// });
+app.post("/campgrounds", function(req, res){
+  var title = req.body.title;
+  var image = req.body.image;
+  var description = req.body.description;
+
+  var newCamp = {title: title, image: image, description: description};
+
+  Campground.create(newCamp, function(err, newCamp){
+    if(err){
+      console.log(err)
+    }else{
+      res.redirect("/");
+    }
+  })
+
+});
+
+// SHOW ROUTE
+app.get("/campgrounds/:id", function(req, res){
+
+  Campground.findById(req.params.id, function(err, foundCamp){
+    if(err){
+      console.log(err);
+    }else{
+        res.render("show", {camp: foundCamp});
+    }
+  })
+});
+
+// EDIT ROUTE
+app.get("/campgrounds/:id/edit", function(req, res){
+  Campground.findById(req.params.id, function(err, foundCamp){
+    if(err){
+      console.log(err)
+    }else{
+      res.render("edit", {camp: foundCamp});
+    }
+  });
+});
+
+// UPDATE ROUTE
+app.put("/campgrounds/:id", function(req, res){
+  Campground.findByIdAndUpdate(req.params.id, req.body.camp, function(err, updatedCamp){
+    if(err){
+      console.log(err)
+    }else{
+      res.redirect(req.params.id);
+    }
+  });
+});
+
+// DELETE ROUTE
+app.delete("/campgrounds/:id", function(req, res){
+  Campground.findByIdAndRemove(req.params.id, function(err){
+    if(err){
+      console.log(err)
+    }else{
+      res.redirect("/campgrounds");
+    }
+  })
+});
+
+
 
 app.listen(3000, function(err){
   if(err){
